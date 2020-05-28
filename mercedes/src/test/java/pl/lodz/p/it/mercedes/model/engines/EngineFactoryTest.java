cd85851e2943182fef6a3ef897ad179dda037f45
Bartłomiej Jencz <bartek.jencz@wp.pl>
2020-05-28 15:24:39 +0200

    Engine factory test

--- mercedes/src/test/java/pl/lodz/p/it/mercedes/model/engines/EngineFactoryTest.java
+++ mercedes/src/test/java/pl/lodz/p/it/mercedes/model/engines/EngineFactoryTest.java
@@ -2,7 +2,11 @@ package pl.lodz.p.it.mercedes.model.engines;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pl.lodz.p.it.mercedes.model.Engine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +20,23 @@ class EngineFactoryTest {
     void tearDown() {
     }
 
-    @Test
-    void getEngine() {
+    @DisplayName("Is engine factory returning good class")
+    @ParameterizedTest(name = "{0} fueltype should return engine {9}")
+    @CsvSource({
+            "'Diesel','Euro 6d ISC-FCM','4x4','4.5', 190.0, 140.0,'4', 4.0, 1950.0, 'class pl.lodz.p.it.mercedes.model.engines.Diesel'",
+            "'Super','Euro 6d ISC-FCM','4x4','4.5', 190.0, 140.0,'4', 4.0, 1950.0, 'class pl.lodz.p.it.mercedes.model.engines.Super'",
+            "'Superplus','Euro 6d ISC-FCM','4x4','4.5', 190.0, 140.0,'4', 4.0, 1950.0, 'class pl.lodz.p.it.mercedes.model.engines.SuperPlus'",
+            "'Electric','Euro 6d ISC-FCM','4x4','4.5', 190.0, 140.0,'4', 4.0, 1950.0, 'class pl.lodz.p.it.mercedes.model.engines.Electric'",
+    })
+    void getEngine(String fuelType, String emissionStandard, String driveType, String fuelConsumption,
+                   Double powerHp, Double powerKw, String cylinder, Double cylinderValves, Double capacity,
+                   String className) {
+
+        Engine engine = EngineFactory.getEngine(fuelType, emissionStandard, driveType, fuelConsumption,
+                powerHp, powerKw, cylinder, cylinderValves, capacity);
+
+        System.out.println(engine.getClass() + ":" + className);
+        assertEquals(engine.getClass().toString(), className);
+
     }
 }
