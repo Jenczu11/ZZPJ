27383d3245234faa74eb342ce8e2dd2a82f4b996
Bartłomiej Jencz <bartek.jencz@wp.pl>
2020-05-28 16:02:01 +0200

    added GetCarWithName test

--- mercedes/src/test/java/pl/lodz/p/it/mercedes/services/CarServiceTest.java
+++ mercedes/src/test/java/pl/lodz/p/it/mercedes/services/CarServiceTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import pl.lodz.p.it.mercedes.TestMongoConfiguration;
 import pl.lodz.p.it.mercedes.TestSuiteExtension;
+import pl.lodz.p.it.mercedes.exceptions.CarNotFoundException;
 import pl.lodz.p.it.mercedes.model.*;
 import pl.lodz.p.it.mercedes.model.engines.EngineFactory;
 import pl.lodz.p.it.mercedes.repositories.CarRepository;
@@ -33,17 +36,21 @@ class CarServiceTest {
     @BeforeEach
     void setUp() {
         List<Car> cars = new ArrayList<>();
-        ArrayList<String> engines = new ArrayList<String>(
+        ArrayList<String> enginesFuelTypes = new ArrayList<String>(
                 Arrays.asList("Diesel", "Super", "SuperPlus","Electric")
         );
-        IntStream.range(0,engines.size()).forEach(i -> {
+        IntStream.range(0,enginesFuelTypes.size()).forEach(i -> {
             String id = Integer.toString(i);
-            Engine engine = EngineFactory.getEngine(engines.get(i), "Euro 6d ISC-FCM", "4x4",
+            String fuelType = enginesFuelTypes.get(i);
+            Engine engine = EngineFactory.getEngine(fuelType, "Euro 6d ISC-FCM", "4x4",
                     "4.5", 190.0, 140.0, "4", 4.0, 1950.0);
+
             Transmission transmission = Transmission.builder().type("8G-DCT").name("auto").build();
+
             CarTechnicalInformation carTechnicalInformation = CarTechnicalInformation.builder()
                     .acceleration(6.9).topSpeed(240.0).doors(4.0).seats(5.0).mass(1620.0)
                     .engine(engine).transmission(transmission).build();
+
             Car car = Car.builder()
                     .modelId(id)
                     .name(String.format("car %d",i))
@@ -56,21 +63,25 @@ class CarServiceTest {
         });
 
 
-        System.out.println("yes");
         repository.insert(cars);
         carService = new CarService(repository);
     }
 
     @AfterEach
     void tearDown() {
+        repository.deleteAll();
     }
 
     @Test
     void addCar() {
     }
 
-    @Test
-    void getCarWithName() {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"car 1","car 2","car 3"})
+    void getCarWithName(String carName) throws CarNotFoundException {
+        assertEquals(carService.getCarWithName(carName).size(),1);
+
     }
 
     @Test
